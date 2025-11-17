32c2600ee896fcf41c484319b5260231f5d7ad13
Murugesh Sujan <murugeshsujan22@gmail.com>
2025-11-17 11:01:28 +0530

    E-mail Attcheched the Company Logo

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace Sujan_Solution_Deployer.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap logo {
+            get {
+                object obj = ResourceManager.GetObject("logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
